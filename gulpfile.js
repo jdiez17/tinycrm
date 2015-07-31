e91f03ca6b5445284618c63a14ca3f9f750095f1
jdiez17 <jose.manuel.diez@gmail.com>
2015-07-31 13:30:15 +0200

    Initial commit of Python and JS

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    clean = require('gulp-clean'),
+    source = require('vinyl-source-stream'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+var output_folder = './tinycrm/static/scripts/js/dist/';
+
+gulp.task('clean', function() {
+    del([output_folder + "main.js"]);
+});
+
+gulp.task('transform', function() {
+    browserify({
+        entries: './tinycrm/static/scripts/jsx/main.jsx',
+        extensions: ['.jsx'],
+        debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(output_folder))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./tinycrm/static/scripts/jsx/*.jsx', ['transform']);
+});
+
+gulp.task('default', function() {
+    gulp.start('transform');
+});
---
+++ tinycrm/static/scripts/jsx/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class App extends React.Component { 
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h2>Hello world from JSX!</h2>
+        );
+    }
+}
+
+export default App;
